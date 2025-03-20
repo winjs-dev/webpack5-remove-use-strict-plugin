b25a47ff2aa71755dc3a0ab4b5833de69bee6d72
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-20 19:23:55 +0800

    chore: upgrade deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  format: 'esm',
   target: 'node20.18',
   clean: true,
-  dts: { transformer: 'oxc' },
+  dts: true,
   platform: 'neutral',
 })
